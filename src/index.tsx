c2b0fd93c37c93c1f0cca6de41fcae26d37245d0
cathiecode <cathiecode@gmail.com>
2022-04-17 01:35:11 +0900

    fix: 初回のマイグレーションが失敗する問題を解消

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,11 @@ import migratorForV0_2 from "./migrator/migrator_0.2";
 (async () => {
   // Migration
   await migratorForV0_2();
-})();
 
-const container = document.createElement("div");
-const regionMain = document.getElementById("region-main");
-if (regionMain) {
-  regionMain.insertBefore(container, regionMain.firstChild);
-  render(<App />, container);
-}
+  const container = document.createElement("div");
+  const regionMain = document.getElementById("region-main");
+  if (regionMain) {
+    regionMain.insertBefore(container, regionMain.firstChild);
+    render(<App />, container);
+  }
+})();
