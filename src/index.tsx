4e37764739aa37b8b542a95c61b579cd93590585
cathiecode <cathiecode@gmail.com>
2022-04-18 03:34:02 +0900

    feat: カスタムコンテナを設定できるように変更

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,21 @@ import migratorForV0_2 from "./migrator/migrator_0.2";
   // Migration
   await migratorForV0_2();
 
-  const container = document.createElement("div");
-  const regionMain = document.getElementById("region-main");
-  if (regionMain) {
+  let container;
+  
+  const customContainer = document.getElementById("timetable-for-hope-placeholder");
+
+  if (customContainer) {
+    container = customContainer;
+  } else {
+    container = document.createElement("div");
+    const regionMain = document.getElementById("region-main");
+    if (!regionMain) {
+      console.error("コンテナのマウントに失敗しました");
+      return;
+    }
     regionMain.insertBefore(container, regionMain.firstChild);
-    render(<App />, container);
   }
+    
+  render(<App />, container);
 })();
